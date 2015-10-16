077077901e18694917b9466cf7ac80d857dddddb
steinaro14 <steinar171@gmail.com>
2015-10-16 23:43:02 +0000

    Added method to handle new lines between numbers

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -29,4 +29,8 @@ public class CalculatorTest {
     	assertEquals(6, Calculator.add("1,2,3"));
     }
 
+	@Test
+    public void testNewLines(){
+    	assertEquals(6, Calculator.add("1/n2,3"));
+    }
 }
